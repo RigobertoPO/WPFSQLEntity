a08c484e719b9a2c057338a547ee42a02d7b7e06
RigobertoPO <rperez@cobach.edu.mx>
2016-10-27 14:35:09 -0500

    Version 1
    Estructura de la aplicación

---
+++ WPFSQLEntity.LogicaNegocio/ContactoMetodos.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFSQLEntity.AccesoDatos;
+
+namespace WPFSQLEntity.LogicaNegocio
+{
+    public class ContactoMetodos
+    {
+        #region Contactos
+        public List<Contacto> ObtenerContactos()
+        {
+            using (var conexion = new ModeloAgendaContainer())
+            {
+                var _contactos = (from c in conexion.Contactos select c);
+                return _contactos.ToList();
+            }             
+        }
+     
+        public bool GuardarContacto(Contacto entidad)
+        {
+            try
+            {
+                ModeloAgendaContainer conexion = new ModeloAgendaContainer();               
+                var consulta = (from c in conexion.Contactos where c.Id == entidad.Id select c).FirstOrDefault();
+                  if (consulta != null)
+                    {
+                        consulta.Nombre = entidad.Nombre;
+                        consulta.Direccion  = entidad.Direccion;
+                        consulta.Telefono = entidad.Telefono;
+                        consulta.FechaNacimiento = entidad.FechaNacimiento;
+                        consulta.CorreoElectronico = entidad.CorreoElectronico;
+                        consulta.Foto = entidad.Foto;
+                        consulta.Activo = entidad.Activo; 
+                        conexion.SaveChanges();
+                        return true;
+                    }
+                  else
+                    {
+                        conexion.Contactos.Add(entidad);
+                        conexion.SaveChanges();
+                        return true;
+                    }
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+     
+        public bool EliminarContacto(Guid id)
+        {
+            try
+            {
+                using (var conexion = new ModeloAgendaContainer())
+                {
+                    var consulta = (from c in conexion.Contactos where c.Id == id select c).FirstOrDefault();
+                    if (consulta != null)
+                    {
+                        conexion.Contactos.Remove(consulta);
+                        conexion.SaveChanges();
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
---
+++ WPFSQLEntity.WPF/Contactos/ContactosUC.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFSQLEntity.LogicaNegocio;
+
+namespace WPFSQLEntity.WPF.Contactos
+{
+    /// <summary>
+    /// Interaction logic for ContactosUC.xaml
+    /// </summary>
+    public partial class ContactosUC : UserControl
+    {
+        public ContactosUC()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            MostrarContactos();
+           
+
+        }
+
+        private void MostrarContactos()
+        {
+            ContactoMetodos _metodos = new ContactoMetodos();
+            var listaContactos = _metodos.ObtenerContactos();
+            foreach (var item in listaContactos)
+            {
+                ContactoLinea _linea = new ContactoLinea();
+                ContactosStackPanel.Children.Add(_linea );
+            }
+        }
+    }
+}
